90c90ae6eeb1696553ad6bc20db031c0dfcba49a
Alex Aksentyev <alexaksentyev@gmail.com>
2016-11-16 14:02:59 +0100

    moved fancy_scientific for common use

--- Reports/SPIN/SPIN_Analysis.R
+++ Reports/SPIN/SPIN_Analysis.R
@@ -129,16 +129,6 @@ cs0mb16 %>% group_by(Soundness, Closeness) %>%
 rbind(mutate(cs0mb12, Year="2012"), mutate(cs0mb16, Year="2016")) -> cs0mb
 
 #### slopes ####
-fancy_scientific <- function(l) {
-  # turn in to character string in scientific notation
-  l <- format(l, scientific = TRUE)
-  # quote the part before the exponent to keep all the digits
-  l <- gsub("^(.*)e", "'\\1'e", l)
-  # turn the 'e+' into plotmath format
-  l <- gsub("e", "%*%10^", l)
-  # return this as an expression
-  parse(text=l)
-}
 slopes12 %>% mutate(
   Group = derivedFactor("Used" = format(Clock, format="%H:%M") < "06:00", .default="Unused")#,
   # Estimate = -1/Estimate, SE = SE*Estimate^2

405222e1810344f6d4df45c576233e2b7b1316cd
Alex Aksentyev <alexaksentyev@gmail.com>
2016-12-21 09:51:34 +0100

    legend key font

--- Reports/Regular/StatRep.R
+++ Reports/Regular/StatRep.R
@@ -5,8 +5,10 @@ source("Utilities.R")
 
 library(reshape2)
 
-lblfnt = 16
-
+lblfnt = 20
+thm = theme_bw() + 
+  theme(axis.text=element_text(size=lblfnt), axis.title=element_text(size=lblfnt), 
+        legend.title=element_text(size=lblfnt), legend.text=element_text(size=lblfnt), legend.position="top")
 
 #### Beam Time ####
 Prec = c(1:10%o%10^(-3:-6))
@@ -15,12 +17,10 @@ Res = 10^(-4:-6); names(Res) <- Res
 ldply(Prec, function(p) c("Precision" = p, BeamTime(Res, Tint, C, 15*60, p)/3600/24)) %>% 
   melt(measure.vars=2:4, variable.name="Resolution", value.name="BeamTime") -> df
 
-ggplot(df, aes(Precision, BeamTime, col=Resolution)) + geom_line() + 
+ggplot(df, aes(Precision, BeamTime, col=Resolution)) + geom_line() + thm +
   scale_y_log10(name="H (days)", minor_breaks=c(1:10%o%10^(-4:+4)), labels=fancy_scientific) + 
-  scale_x_log10(name=expression(sigma[EA[yy]]~"(a.u.)", minor_breaks=Prec, labels=fancy_scientific)) + 
-  theme_bw() +
-  theme(legend.position="top", axis.text=element_text(size=lblfnt), axis.title=element_text(size=lblfnt))
-
+  scale_x_log10(name=expression(sigma[EA[yy]]~"(a.u.)"), minor_breaks=Prec, labels=fancy_scientific) +
+  scale_color_discrete(labels=fancy_scientific)
 
 #### State Time ####
 vmbe <- function(var.meas,var.slope,h){
@@ -37,11 +37,10 @@ hmin = (24*Tint* vd/vb)^(1/3); hmin/60
 ldply(seq(100,3600,100), function(h) c("h" = h, sqrt(vmbe(vd, vb, h)))) %>% 
   melt(measure.vars=1+seq(length(sdd)), variable.name="Resolution",value.name="SDQI") -> df
 
-ggplot(df, aes(h, SDQI, col=Resolution)) + geom_line() + theme_bw() + 
+ggplot(df, aes(h, SDQI, col=Resolution)) + geom_line() + thm +
   scale_y_log10(name=expression(sigma[EA[yy]]~"(a.u.)"), breaks=10^(-4:-1), labels=fancy_scientific) +
   scale_x_continuous(name="h (sec)", minor_breaks=seq(100,3600,100)) +
-  theme(legend.position="top", axis.text=element_text(size=lblfnt), axis.title=element_text(size=lblfnt))
-
+  scale_color_discrete(labels=fancy_scientific)
 #### slope variation from rho ####
 Rho.ini = Rho.on
 rate = 1.157e-6
@@ -52,6 +51,5 @@ ldply(seq(3600), function(n){
   c("h" = n, "vb" = var(lam))
 }) -> df
 
-ggplot(df, aes(h,vb)) + geom_line() + theme_bw() + 
-  scale_y_log10(limits=c(1e-24, 1e-16)) + labs(x="h (sec)", y=expression(sigma[beta]^2~(sec^-2))) +
-  theme(legend.position="top", axis.text=element_text(size=lblfnt), axis.title=element_text(size=lblfnt))
+ggplot(df, aes(h,vb)) + geom_line() + thm + labs(x="h (sec)", y=expression(sigma[beta]^2~(sec^-2))) +
+  scale_y_log10(limits=c(1e-24, 1e-16), labels=fancy_scientific)
